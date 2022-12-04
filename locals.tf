37fb25cb5697b9172427a453e511e8491b634981
EmmaINSA <emmaneiss2@gmail.com>
2022-12-03 23:37:13 -0500

    Tf fmt

--- locals.tf
+++ locals.tf
@@ -3,25 +3,25 @@ locals {
     [
       {
         description = "Allow all HTTP traffic"
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
         cidr_blocks = "0.0.0.0/0"
       },
       {
         description = "Allow all HTTPS traffic"
-        from_port = 443
-        to_port = 443
-        protocol = "tcp"
+        from_port   = 443
+        to_port     = 443
+        protocol    = "tcp"
         cidr_blocks = "0.0.0.0/0"
       }
     ],
-    [ for ip in var.ssh_allowed_ips : 
+    [for ip in var.ssh_allowed_ips :
       {
         description = "IP-restricted SSH"
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
         cidr_blocks = "${ip}/32"
       }
     ]
@@ -31,9 +31,9 @@ locals {
     {
       # TODO: see if SSH, HTTP & HTTPS can be enough eventually
       description = "Allow all egress traffic"
-      from_port = 0
-      to_port = 0
-      protocol = "all"
+      from_port   = 0
+      to_port     = 0
+      protocol    = "all"
       cidr_blocks = "0.0.0.0/0"
     }
   ]
--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~>4.31.0"
     }
   }
@@ -22,11 +22,11 @@ module "vpc" {
 
   name = "${var.app}-vpc"
 
-  azs = var.vpc_azs
-  cidr = var.vpc_cidr
+  azs            = var.vpc_azs
+  cidr           = var.vpc_cidr
   public_subnets = var.public_subnets
 
-  create_igw = true
+  create_igw              = true
   map_public_ip_on_launch = true
 }
 
@@ -35,11 +35,11 @@ module "security_group" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "~>4.16.2"
 
-  name = "${var.app}-sg-ec2"
+  name   = "${var.app}-sg-ec2"
   vpc_id = module.vpc.vpc_id
 
   ingress_with_cidr_blocks = local.sg_ingress
-  egress_with_cidr_blocks = local.sg_egress
+  egress_with_cidr_blocks  = local.sg_egress
 }
 
 # ============================= EC2
@@ -47,12 +47,12 @@ module "ec2" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "~>4.2.1"
 
-  name = "${var.app}-ec2"
-  ami = data.aws_ami.amazon_linux_ami.image_id
+  name          = "${var.app}-ec2"
+  ami           = data.aws_ami.amazon_linux_ami.image_id
   instance_type = var.ec2_instance_type
 
   vpc_security_group_ids = [module.security_group.security_group_id]
-  subnet_id = module.vpc.public_subnets[0]
+  subnet_id              = module.vpc.public_subnets[0]
 
   key_name = aws_key_pair.key_pair.key_name
 
@@ -63,7 +63,7 @@ module "ec2" {
 
 # ============================= KEY PAIR
 resource "aws_key_pair" "key_pair" {
-  key_name = "${var.app}-ec2-key-pair"
+  key_name   = "${var.app}-ec2-key-pair"
   public_key = tls_private_key.private_key.public_key_openssh
 }
 
@@ -76,23 +76,23 @@ data "aws_ami" "amazon_linux_ami" {
   most_recent = true
 
   filter {
-    name = "owner-alias"
+    name   = "owner-alias"
     values = ["amazon"]
   }
   filter {
-    name = "name"
+    name   = "name"
     values = ["amzn2-ami-hvm*"]
   }
   filter {
-    name = "root-device-type"
+    name   = "root-device-type"
     values = ["ebs"]
   }
   filter {
-    name = "architecture"
+    name   = "architecture"
     values = ["x86_64"]
   }
   filter {
-    name = "virtualization-type"
+    name   = "virtualization-type"
     values = ["hvm"]
   }
 }
